b438a6c655abd6ba1fc63cfac3e62762a1fc9068
Nicholas Partridge <nick@Macintosh.local>
2008-03-03 20:00:15 +1000

    Fixed up some of the build text, and added license text for rails bundle code

--- Support/bin/htmlize_ant.rb
+++ Support/bin/htmlize_ant.rb
@@ -10,17 +10,17 @@
 bundle_lib = ENV['TM_BUNDLE_SUPPORT'] + '/lib'
 $LOAD_PATH.unshift(bundle_lib) if ENV['TM_BUNDLE_SUPPORT'] and !$LOAD_PATH.include?(bundle_lib)
 
-require 'text_mate'
+require 'rails/text_mate'
 require "#{ENV["TM_SUPPORT_PATH"]}/lib/escape"
 require "#{ENV["TM_SUPPORT_PATH"]}/lib/web_preview"
 
 Dir.chdir TextMate.project_directory
 task = ARGV.shift
 command = "ant #{task}"
-puts html_head(:window_title => "#{task} — RakeMate", :page_title => 'RakeMate', :sub_title => 'Rake')
+puts html_head(:window_title => "#{task} — Ant Build", :page_title => 'Build', :sub_title => '')
 puts <<-HTML
-    <div id="report_title">Rake Report</div>
-    <div id="rake_command">#{command}</div>
+    <div id="report_title">Build Output</div>
+    <div id="rake_command"><code>#{command}</code></div>
     <div>
 			<pre><!-- <strong>RakeMate</strong> -->
 <div style="white-space: normal; -khtml-nbsp-mode: space; -khtml-line-break: after-white-space;">
@@ -31,16 +31,9 @@ report = ""
 compile_error_re = /(\/.*?):(\d+):\s*(.*)$/
 output.to_a.each do |line|
   if (match = line.match(compile_error_re)) then
-    fn = match[1]
-    if defined?(proj_dir) and fn.startswith(proj_dir)
-      short_name = fn[len(proj_dir)]
-    else
-      short_name = fn
-      colInd = 0
-    end
-    report += '%s<a href="txmt://open?url=file://%s&line=%s&column=%d">%s:%s: %s</a>' % [
-        match[0], fn, match[2], colInd, short_name, match[2], match[3]]
-    report += "<b>#{line}</b>" + "<br />"
+    report += '%s<a href="txmt://open?url=file://%s&line=%s&column=%d">%s:%s: %s</a><br />' % [
+        match.pre_match, match[1], match[2], 0, match[1], match[2], match[3]
+    ]
   else
     report += line + "<br />"
   end
