d3395189b9115ee0ba3443d18db9521126401bac
Alcides Fonseca <me@alcidesfonseca.com>
2008-09-20 16:09:56 +0100

    Added Hello Word Template and Changed the shortcut for ant build

---
+++ Templates/Scala.tmTemplate/main.scala
@@ -0,0 +1,12 @@
+//
+//  ${TM_NEW_FILE_BASENAME}
+//
+//  Created by ${TM_USERNAME} on ${TM_DATE}.
+//  Copyright (c) ${TM_YEAR} ${TM_ORGANIZATION_NAME}. All rights reserved.
+//
+
+object HelloWorld {
+	def main(args: Array[String]) {
+		println("Hello, world!")
+	}
+}
